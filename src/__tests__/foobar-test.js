77c011c624c6764fc51358ce5630c392cb34668a
SophGits <sophie.jiffy@gmail.com>
2016-02-23 16:03:25 +0000

    Write a 'test' test with jest and one to check a node has rendered into the DOM.

---
+++ src/__tests__/foobar-test.js
@@ -0,0 +1,17 @@
+jest.dontMock('../foobar');
+var React = require('react'),
+    ReactDOM = require('react-dom'),
+    TestUtils = require('react-addons-test-utils'),
+    Foobar = require('../foobar');
+
+describe('foobar', function() {
+  it('renders \'Hello, World\'', function() {
+    var helloworld = TestUtils.renderIntoDocument(
+      <Foobar />
+    );
+
+    var node = ReactDOM.findDOMNode(helloworld);
+
+    expect(node.textContent).toEqual('Hello, world');
+  })
+})
